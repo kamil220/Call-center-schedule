0685e5caf31672d9efa81493e38001edf48c7eb4
Kamil Lazarz <kamillazarz@gmail.com>
2025-04-14 18:15:30 +0200

    chore: remove database service from Docker configuration and update user table schema to use JSON for roles; add UserFixtures for user data population

--- migrations/Version20250414153144.php
+++ migrations/Version20250414153144.php
@@ -21,7 +21,7 @@ final class Version20250414153144 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            CREATE TABLE users (id VARCHAR(255) NOT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, roles CLOB NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, active BOOLEAN NOT NULL, PRIMARY KEY(id))
+            CREATE TABLE users (id VARCHAR(255) NOT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, active BOOLEAN NOT NULL, PRIMARY KEY(id))
         SQL);
         $this->addSql(<<<'SQL'
             CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON users (email)
